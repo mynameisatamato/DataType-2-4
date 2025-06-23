0665430e9a3719992789caf1ca903b830ea3c6c3
Worapol Thangban <worpolk24@gmail.com>
2025-06-23 12:18:48 +0700

    first commit

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,24 @@
+fun main(){
+    println("hello PSC")
+    var a: Int  = 67;
+    val A: Int = 33;
+    var Ko: Int = a+A;
+    println(a)
+    println(A)
+    println(" $A + $a = $Ko ")
+    Kop()
+}
+fun Kop(){
+    var KStr: String = "Class24"
+    var Kdo: Double = 500.00
+    var Kint: Int = 10_000
+
+    println("$Kint \n $Kdo \n $KStr")
+
+    var x: Int = 10
+    var y: Int = 10
+    x++
+    println(x) // 11
+    ++y
+    println(y) // 11
+}
